b559cf337ca6ea8d77ef98efc7eb54a4902ab614
Gyeongjae Kim <kimg009263@gmail.com>
2021-06-04 16:19:24 +1200

    Add project files.

---
+++ Matrix_Practice_0106/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Matrix_Practice_0106
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbRow1 = new System.Windows.Forms.TextBox();
+            this.tbColumn1 = new System.Windows.Forms.TextBox();
+            this.tbRow2 = new System.Windows.Forms.TextBox();
+            this.tbColumn2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnCreate1 = new System.Windows.Forms.Button();
+            this.btnCreate2 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Row";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Column";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Row";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(455, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Column";
+            // 
+            // tbRow1
+            // 
+            this.tbRow1.Location = new System.Drawing.Point(215, 43);
+            this.tbRow1.Name = "tbRow1";
+            this.tbRow1.Size = new System.Drawing.Size(100, 25);
+            this.tbRow1.TabIndex = 4;
+            // 
+            // tbColumn1
+            // 
+            this.tbColumn1.Location = new System.Drawing.Point(215, 95);
+            this.tbColumn1.Name = "tbColumn1";
+            this.tbColumn1.Size = new System.Drawing.Size(100, 25);
+            this.tbColumn1.TabIndex = 5;
+            // 
+            // tbRow2
+            // 
+            this.tbRow2.Location = new System.Drawing.Point(540, 43);
+            this.tbRow2.Name = "tbRow2";
+            this.tbRow2.Size = new System.Drawing.Size(100, 25);
+            this.tbRow2.TabIndex = 6;
+            // 
+            // tbColumn2
+            // 
+            this.tbColumn2.Location = new System.Drawing.Point(540, 95);
+            this.tbColumn2.Name = "tbColumn2";
+            this.tbColumn2.Size = new System.Drawing.Size(100, 25);
+            this.tbColumn2.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(131, 234);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(460, 234);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 9;
+            // 
+            // btnCreate1
+            // 
+            this.btnCreate1.Location = new System.Drawing.Point(296, 184);
+            this.btnCreate1.Name = "btnCreate1";
+            this.btnCreate1.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate1.TabIndex = 10;
+            this.btnCreate1.Text = "Create";
+            this.btnCreate1.UseVisualStyleBackColor = true;
+            this.btnCreate1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCreate2
+            // 
+            this.btnCreate2.Location = new System.Drawing.Point(623, 184);
+            this.btnCreate2.Name = "btnCreate2";
+            this.btnCreate2.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate2.TabIndex = 11;
+            this.btnCreate2.Text = "Create";
+            this.btnCreate2.UseVisualStyleBackColor = true;
+            this.btnCreate2.Click += new System.EventHandler(this.btnCreate2_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(215, 508);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 27;
+            this.dataGridView3.Size = new System.Drawing.Size(365, 214);
+            this.dataGridView3.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(137, 508);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Result";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(205, 438);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 23);
+            this.btnPlus.TabIndex = 14;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(302, 438);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 23);
+            this.btnMinus.TabIndex = 15;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(401, 438);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 16;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(493, 438);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(110, 23);
+            this.btnDivide.TabIndex = 17;
+            this.btnDivide.Text = "Transpose";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(625, 483);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 18;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 783);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.btnCreate2);
+            this.Controls.Add(this.btnCreate1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbColumn2);
+            this.Controls.Add(this.tbRow2);
+            this.Controls.Add(this.tbColumn1);
+            this.Controls.Add(this.tbRow1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbRow1;
+        private System.Windows.Forms.TextBox tbColumn1;
+        private System.Windows.Forms.TextBox tbRow2;
+        private System.Windows.Forms.TextBox tbColumn2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnCreate1;
+        private System.Windows.Forms.Button btnCreate2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Matrix_Practice_0106/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matrix_Practice_0106
+{
+    public partial class Form1 : Form
+    {
+        public int R, C,a,b,c;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(tbColumn1.Text) == Convert.ToInt32(tbRow2.Text))
+            {
+                //Matrix creation
+                dataGridView3.RowCount = Convert.ToInt32(tbRow1.Text);
+                dataGridView3.ColumnCount = Convert.ToInt32(tbColumn2.Text);
+                dataGridView3.AutoResizeColumns();
+                dataGridView3.AutoResizeRows();
+
+                //Matrix Calculation
+                try
+                {
+                    foreach (DataGridViewRow row in this.dataGridView1.Rows)
+                    {
+                        int R1 = row.Index;
+                        {
+                            for (b = 0; b < dataGridView2.ColumnCount; b++)
+                            {
+                                a = 0;
+                                for (c = 0; c < dataGridView2.RowCount; c++)
+                                {
+                                    decimal D = Convert.ToDecimal(dataGridView1.Rows[R1].Cells[c].Value.ToString());
+                                    decimal E = Convert.ToDecimal(dataGridView2.Rows[c].Cells[b].Value.ToString());
+
+                                    decimal A = D * E;
+                                    dataGridView3.Rows[R1].Cells[b].Value = decimal.Parse(A.ToString());
+                                    dataGridView3.Rows[R1].Cells[b].Value = a + Convert.ToDouble(dataGridView3.Rows[R1].Cells[b].Value);
+                                    a = Convert.ToInt32(dataGridView3.Rows[R1].Cells[b].Value);
+
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Wrong");
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create rows
+            if (tbRow1.Text != "" & tbColumn1.Text != "")
+            {
+                dataGridView1.RowCount = (Convert.ToInt32(tbRow1.Text));
+                R = dataGridView1.RowCount;
+                dataGridView1.AllowUserToResizeColumns = true;
+                dataGridView1.AutoResizeRows();
+
+                dataGridView1.ColumnCount = (Convert.ToInt32(tbColumn1.Text));
+                C = dataGridView1.ColumnCount;
+                dataGridView1.AutoResizeColumns();
+            }
+            else
+            {
+                MessageBox.Show("Please fill in the blank");
+            }
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void btnCreate2_Click(object sender, EventArgs e)
+        {
+            if (tbRow1.Text != "" & tbColumn1.Text != "")
+            {
+                dataGridView2.RowCount = (Convert.ToInt32(tbRow2.Text));
+                R = dataGridView2.RowCount;
+                dataGridView2.AllowUserToResizeColumns = true;
+                dataGridView2.AutoResizeRows();
+
+                dataGridView2.ColumnCount = (Convert.ToInt32(tbColumn2.Text));
+                C = dataGridView2.ColumnCount;
+                dataGridView2.AutoResizeColumns();
+            }
+            else
+            {
+                MessageBox.Show("Please fill in the blank");
+            }
+
+        }
+    }
+}
